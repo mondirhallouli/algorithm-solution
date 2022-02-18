333d227f523aebd6b9a4170fc3102bf63af2a1f5
mondirhallouli <halloulim@gmail.com>
2022-02-18 20:46:26 +0100

    Palindrome number challenge

---
+++ palindrome-number.js
@@ -0,0 +1,15 @@
+/*
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+*/
+
+// var isPalindrome = function(x) {
+//   let strNum = String(x);
+//   let revNum = Number(strNum.split('').reverse().join(''));
+//   return revNum == x;
+// };
+
+console.log(isPalindrome(121));
