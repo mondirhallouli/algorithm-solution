487f50f3681358b1cd498dbc4904491166e0e56e
mondirhallouli <halloulim@gmail.com>
2022-09-01 13:11:34 +0100

    LC: Palindrome linked list, constant space solution

--- leetcode/isPalindrome.test.js
+++ leetcode/isPalindrome.test.js
@@ -11,23 +11,60 @@
     Space: O(n), n: the number of nodes that take space in the array in order to be compared
 */
 
-let isPalindrome = function(head) {
-    if (!head) return false;
-    let list = [];
-    let current = head;
-    while (current) {
-        list.push(current.val);
-        current = current.next;
-    }
-    let start = 0, end = list.length -1;
-    while (start <= end) {
-        if (list[start] !== list[end]) return false;
-        start += 1;
-        end -= 1;
-    }
-    return true;
-};
+// let isPalindrome = function(head) {
+//     if (!head) return false;
+//     let list = [];
+//     let current = head;
+//     while (current) {
+//         list.push(current.val);
+//         current = current.next;
+//     }
+//     let start = 0, end = list.length -1;
+//     while (start <= end) {
+//         if (list[start] !== list[end]) return false;
+//         start += 1;
+//         end -= 1;
+//     }
+//     return true;
+// };
 
+/* 
+    TIME AND SPACE:
+    --------------
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+let isPalindrome2 = function(head) {
+    let slow = fast = head, previouNode, nextNode;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    previousNode = slow;
+    slow = slow.next;
+    previousNode.next = null;
+
+    while (slow) {
+        nextNode = slow.next;
+        slow.next = previousNode;
+        previousNode = slow;
+        slow = nextNode;
+    }
+
+    fast = head;
+    slow = previousNode;
+
+    while (slow) {
+        if (slow.val != fast.val) return false;
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return true;
+}
 
 // TESTS
 describe("Tests that should return true", () => {
